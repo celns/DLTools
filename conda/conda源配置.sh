d3a585f6fee4e0eba81a433b1618de6b140045f5
celns <celns@outlook.com>
2019-12-08 14:40:47 +0800

    conda源配置
    Signed-off-by: celns <celns@outlook.com>

--- conda/conda源配置.sh
+++ conda/conda源配置.sh
@@ -3,16 +3,16 @@
 #
 
 #查看当前源
-conda --show-sources
+conda config --show-sources
 
 #删除所有配置的源，即重置为默认源
-conda --remove channels
+conda config --remove channels
 
 #删除特定源,这里Lunaria就是特定源地址
-conda --remove channels Lunaria
+conda config --remove channels Lunaria
 
 #添加指定源
-conda --add channels Lunaria
+conda config --add channels Lunaria
 
 #设置在安装包的时候，显示源url
-conda --set show_channel-urls yes
+conda config --set show_channel-urls yes
